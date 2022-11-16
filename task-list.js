68b78458aa4c7699c3e5b91a7c088845a0eea331
Saurinnn <44335507+Saurinnn@users.noreply.github.com>
2022-11-16 17:56:54 +0900

    サンプルデータを挿入

--- task-list.js
+++ task-list.js
@@ -10,6 +10,21 @@ const taskList =JSON.parse(localStorage.getItem('tasks'));
 let tasks = taskList || [];
 displayTaskList();
 
+if(tasks.length === 0){
+//サンプルデータを追加
+function addSample(){
+  const task = {
+    month: 	"2021-07", //実施月
+    status: "済", //進捗
+    title: "A 社経営統合プロジェクト", //タイトル
+    detail: '経営統合に伴う業務プロセス統合プロジェクト。\nプロジェクトリーダー（メンバー４人）として担当。\nＱＤＣ目標いずれも達成。ＣＳ評価で５をいただいた。' //概要
+   }
+  addTask(task);
+}
+
+addSample(); 
+}
+
 submitButton.onclick = () => {
  //taskオブジェクトにタスクの情報を登録する
  const task = {
@@ -46,7 +61,7 @@ function deleteTask(deleteIndex){
 function displayTaskList(){
   taskListTbody.innerText="";
   for(let i = 0;i < tasks.length;i++){
-    const task = taskList[i];
+    const task = tasks[i];
     // テーブル行を作る
     const taskTr = document.createElement('tr');
     const monthTd = document.createElement('td');
@@ -74,16 +89,3 @@ function displayTaskList(){
   }
 }
 
-/*
-//サンプルデータを追加
-function addSample(){
-  const task = {
-    month: 	"2021-07", //実施月
-    status: "済", //進捗
-    title: "A 社経営統合プロジェクト", //タイトル
-    detail: '経営統合に伴う業務プロセス統合プロジェクト。\nプロジェクトリーダー（メンバー４人）として担当。\nＱＤＣ目標いずれも達成。ＣＳ評価で５をいただいた。' //概要
-   }
-  addTask(task);
-}
-
-addSample(); */
